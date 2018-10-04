ff628cb59b488786d210e48f8a66e15380caa332
cycpe950609 <cycpe950609@gmail.com>
2018-10-04 20:02:53 +0800

    Main Window design

--- PySnake/PySnake.py
+++ PySnake/PySnake.py
@@ -5,7 +5,10 @@ from System.Windows import Application, Window
 class MyWindow(Window):
     def __init__(self):
         wpf.LoadComponent(self, 'PySnake.xaml')
-    
+        
+    def MenuItem_Click(self, sender, e):
+        pass
+
 
 if __name__ == '__main__':
     Application().Run(MyWindow())
